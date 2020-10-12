9c17c66721403ecb73151ecdd2eb1fc8e448e59a
Ben Okello Mwaka <mwakaben@gmail.com>
2020-10-12 21:49:44 +0300

    Start Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -41,5 +41,12 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view){
+        Uri telephoneNumber =Uri.parse("tel:0123456789");
+        Intent dail = new Intent(Intent.ACTION_DIAL,telephoneNumber);
+
+        startActivity(dail);
+
+    }
     // ***
 }
